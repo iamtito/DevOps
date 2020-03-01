e9ca44563470b877d2ad5175e4924f61feb01f49
iamtito <tbola45@yahoo.com>
2020-03-01 03:02:08 -0500

    Create ec2-instance.tf

---
+++ Terraform/ec2-instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region                  = "us-east-1" 
+}
+resource "aws_instance" "server" {
+  ami = "ami-2d39803a"
+  instance_type = "t2.micro"
+  tags {
+      Name          = "server-one"
+      Environment   = "Production"
+      App           = "ecommerce"
+  }
+}
